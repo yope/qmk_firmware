0c3b41f3298a9aab0912df7f9cf6f572b17998ae
David Jander <david@protonic.nl>
2026-03-23 08:58:08 +0100

    Add initial support for version 3 of RK R65 keyboard PCB
    I called this "v3", because I have found 2 different prior versions of
    this PCB. It looks like this might actually be the same PCB as the
    wireless version of the R65 but with the radio parts not populated.
    In any case this version seems to be very different than the previous
    ones, since the pin allocation has changed quite a lot.
    The RGB matrix is still not correct in this initial version and the
    rotary encode is not functioning, but the keyboard is at least usable.
    
    Signed-off-by: David Jander <david@protonic.nl>

---
+++ keyboards/rk/r65v3/config.h
@@ -0,0 +1,36 @@
+// Based on kb_rk_r65:
+// Copyright 2024 DV (@iamdanielv)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define LED_WIN_LOCK_PIN C5
+#define LED_MAC_PIN B0
+
+#define USB_POLLING_INTERVAL_MS 1
+
+/* SPI */
+#define SPI_DRIVER SPIDQ
+#define SPI_SCK_PIN B3
+#define SPI_MOSI_PIN B5
+#define SPI_MISO_PIN B4
+
+/* Flash */
+#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
+#define WEAR_LEVELING_LOGICAL_SIZE (WEAR_LEVELING_BACKING_SIZE / 2)
+
+/* RGB Matrix*/
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+/* WS2812 */
+#define WS2812_SPI_DRIVER SPIDM2
+#define WS2812_SPI_DIVISOR 30
+
+/* Default Effects */
+#define RGB_MATRIX_DEFAULT_ON true
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
+
+#define PALETTEFX_ENABLE_ALL_EFFECTS
+#define PALETTEFX_ENABLE_ALL_PALETTES
---
+++ keyboards/rk/r65v3/r65v3.c
@@ -0,0 +1,32 @@
+// Based on r65.c by DV:
+// Copyright 2024 DV (@iamdanielv).
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "quantum.h"
+
+#include QMK_KEYBOARD_H
+
+#define LED_ENABLE_PIN A5
+
+void keyboard_pre_init_kb(void) {
+    gpio_set_pin_output(LED_ENABLE_PIN);
+    gpio_write_pin_high(LED_ENABLE_PIN);
+
+    gpio_set_pin_output(LED_WIN_LOCK_PIN);
+    gpio_write_pin_high(LED_WIN_LOCK_PIN);
+
+    gpio_set_pin_output(LED_MAC_PIN);
+    gpio_write_pin_high(LED_MAC_PIN);
+
+    keyboard_pre_init_user();
+}
+
+void housekeeping_task_kb(void){
+
+    if(keymap_config.no_gui)
+    {
+        gpio_write_pin_low(LED_WIN_LOCK_PIN);
+    } else {
+        gpio_write_pin_high(LED_WIN_LOCK_PIN);
+    }
+}
